a1307ff32019ff48849abef7ac4cdd5cc00d5def
Favour <toluwaniokunowo@gmail.com>
2022-09-19 22:40:39 +0100

    add links to auth form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,38 +1,29 @@
 <x-guest-layout>
     <a href="/" class="flex justify-center items-center">
-        <x-application-logo class="w-20 h-20 fill-current text-gray-500"/>
+        <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
     </a>
 
     <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')"/>
+    <x-auth-session-status class="mb-4" :status="session('status')" />
 
     <!-- Validation Errors -->
-    <x-auth-validation-errors class="mb-4 mt-4" :errors="$errors"/>
+    <x-auth-validation-errors class="mb-4 mt-4" :errors="$errors" />
 
     <form method="POST" action="{{ route('login') }}" class="mt-4">
-    @csrf
+        @csrf
 
         <!-- Email Address -->
         <div>
-            <x-label for="email" :value="__('Email')"/>
-            <x-input type="email"
-                     name="email"
-                     id="email"
-                     value="{{ old('email') }}"
-                     required
-                     autofocus
-            />
+            <x-label for="email" :value="__('Email')" />
+            <x-input type="email" name="email" id="email" value="{{ old('email') }}" required
+                autofocus />
         </div>
 
         <!-- Password -->
         <div class="mt-3">
-            <x-label for="password" :value="__('Password')"/>
-            <x-input type="password"
-                     name="password"
-                     id="password"
-                     required
-                     autocomplete="current-password"
-            />
+            <x-label for="password" :value="__('Password')" />
+            <x-input type="password" name="password" id="password" required
+                autocomplete="current-password" />
         </div>
 
         <div class="flex justify-between mt-4">
@@ -46,16 +37,22 @@
 
             <div>
                 @if (Route::has('password.request'))
-                    <a class="block text-sm fontme text-indigo-700 hover:underline"
-                       href="{{ route('password.request') }}">{{ __('Forgot your password?') }}</a>
+                <a class="block text-sm fontme text-indigo-700 hover:underline"
+                    href="{{ route('password.request') }}">{{ __('Forgot your password?') }}</a>
                 @endif
             </div>
         </div>
 
-        <div class="mt-6">
+
+        <div class="flex flex-col items-end mt-4">
             <x-button class="w-full">
                 {{ __('Log in') }}
             </x-button>
+
+            <a class="mt-4 text-sm text-gray-600 underline hover:text-gray-900"
+                href="{{ route('register') }}">
+                {{ __('Not registered?') }}
+            </a>
         </div>
 
     </form>
