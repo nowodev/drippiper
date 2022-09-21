c064c7f585d5e926058f692de328c4aa1d4568cd
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-09-21 14:55:07 +0100

    feats: finished footer template

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,14 +1,50 @@
 <footer>
-    <div class="container mx-auto  px-3 py-6 flex justify-between items-center">
-        <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem atque necessitatibus esse minima at
-            consequatur magnam culpa tempore numquam cupiditate laudantium nostrum vel cum dolore excepturi, vero
-            provident facere pariatur aut dolores animi aliquid? Inventore eveniet distinctio a quae nam dicta, itaque
-            iusto, deserunt doloribus ipsam accusamus ratione veritatis, quo velit cum aperiam eligendi atque modi
-            dolore voluptatem accusantium id ipsa eum in! Sit voluptatibus nam nobis, laboriosam quam veniam aliquam
-            voluptatum molestias illum nihil, natus repellat odit! Quam provident aperiam dignissimos eos optio
-            repudiandae. Sapiente ipsa labore, ducimus molestiae architecto aut, incidunt ullam ad neque distinctio
-            exercitationem expedita in!
+    <div class="mx-auto px-12 py-6 md:flex justify-between items-center bg-black text-white">
+        <div class="md:w-1/3 mb-4 md:mb-0">
+            <h3>Logo</h3>
+            <p class="font-bold text-xl"> Piper Wears</p>
+            <p class="mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod aut suscipit tempora vel
+                obcaecati
+                laboriosam ipsum omnis illum reiciendis possimus aliquid repellat, laudantium necessitatibus deserunt?
+            </p>
         </div>
+        <div class="mb-4 md:mb-0">
+            <h3 class=" font-bold text-xl ">
+                Explore
+            </h3>
+            <ul>
+                <li>
+                    <a href=""> About</a>
+                </li>
+                <li>
+                    <a href="#  ">Contact</a>
+                </li>
+                <li>
+                    <a href="@">FAQs</a>
+                </li>
+            </ul>
+        </div>
+        <div>
+            <h3>
+                Newsletter
+            </h3>
+            <input type="text" name="newsletter" placeholder="Enter Email" class="rounded block">
+            <button class="btn bg-white text-black px-3 py-2 mt-2 rounded cursor-pointer">Subscribe</button>
+        </div>
+
     </div>
+    <!-- social media footer -->
+    <section>
+        <div class="text-center bg-white text-black flex justify-center space-x-6 py-4">
+            <div>
+                &copy; piperwears 2022
+            </div>
+            <div class="">
+                Instagram
+            </div>
+            <div>
+                Twitter
+            </div>
+        </div>
+    </section>
 </footer>
