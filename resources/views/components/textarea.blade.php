fe5f8bc4718d2e79f263148e6808cc64ee30eb59
Favour <toluwaniokunowo@gmail.com>
2022-09-21 01:22:51 +0100

    fix: textarea not retaining old value

--- resources/views/components/textarea.blade.php
+++ resources/views/components/textarea.blade.php
@@ -1,3 +1,5 @@
 @props(['disabled' => false])
 
-<textarea {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'block mt-1 w-full rounded-md form-input focus:border-indigo-600']) !!}></textarea>
+<textarea {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'block mt-1 w-full rounded-md form-input focus:border-indigo-600']) !!}>
+{{ $slot }}
+</textarea>
