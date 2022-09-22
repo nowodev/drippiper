2c2606807b50cd01b4fd9c010dc82c7ee1126f33
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-09-22 11:31:07 +0100

    feats: updated footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,8 +1,14 @@
 <footer>
     <div class="mx-auto px-12 py-6 md:flex justify-between items-center bg-black text-white">
         <div class="md:w-1/3 mb-4 md:mb-0">
-            <h3>Logo</h3>
-            <p class="font-bold text-xl"> Piper Wears</p>
+            <a href="#" class="flex title-font font-medium items-center md:justify-start justify-center text-gray-800">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round"
+                    stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
+                    viewBox="0 0 24 24">
+                    <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+                </svg>
+                <span class="ml-3 text-xl font-bold text-white">Piper Wears</span>
+            </a>
             <p class="mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod aut suscipit tempora vel
                 obcaecati
                 laboriosam ipsum omnis illum reiciendis possimus aliquid repellat, laudantium necessitatibus deserunt?
@@ -51,6 +57,7 @@
             <p class="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">
                 &copy piperwears 2022
             </p>
+            <!--- Social Media Icon -->
             <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
                 <a class="text-gray-500">
                     <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
