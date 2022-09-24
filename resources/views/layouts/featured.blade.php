b3869df1e662b7f84ef3de17b43d16d90830705c
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-09-24 09:26:03 +0100

    feats: updated featured product

--- resources/views/layouts/featured.blade.php
+++ resources/views/layouts/featured.blade.php
@@ -16,6 +16,18 @@
                     alt="" class="h-96 duration-500 group-hover:opacity-0">
                 <img src="https://cdn.shopify.com/s/files/1/0066/2326/4828/products/image_67c584d0-d8d4-4452-8bd5-a360370b647d_360x.jpg?v=1662634229"
                     alt="" class="h-96 absolute top-0 z-[-1]">
+                <p class="absolute px-3 group-hover:bottom-28 text-center bg-black text-white duration-700">Lorem ipsum,
+                    dolor sit
+                    amet
+                    consectetur
+                    adipisicing
+                    elit.
+                    Optio
+                    placeat
+                    labore
+                    ullam
+                    dolorem. Nisi,
+                    harum.</p>
                 <button class="absolute px-5 py-2 bg-black text-white font-semibold rounded
             left-1/4 bottom-[-65px] group-hover:bottom-14 duration-700">VIEW PRODUCT</button>
 
@@ -25,8 +37,6 @@
                     <path stroke-linecap="round" stroke-linejoin="round"
                         d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
                 </svg>
-
-
             </div>
 
 
