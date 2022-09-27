8e79223ba1ae8647800244a5b956183affd99850
Favour <toluwaniokunowo@gmail.com>
2022-09-27 12:16:59 +0100

    update font

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
 
         <!-- Fonts -->
         <link rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+            href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;600;700;800;900&display=swap">
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
