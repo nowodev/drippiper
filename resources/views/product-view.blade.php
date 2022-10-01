cad9921b48e093000fb732726123294979478143
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-10-01 20:28:49 +0100

    feats: updated display image

--- resources/views/product-view.blade.php
+++ resources/views/product-view.blade.php
@@ -126,24 +126,39 @@
                 </div>
             </div>
         </div>
+    </section>
 
+    <section>
         <!-- other products section -->
-        <div class="w-full md:flex justify-center items-center mb-24">
+        <div class="w-full md:flex justify-center items-center md:mb-24">
             <div class="md:flex md:space-x-4 mb-4">
-                <div class="relative cursor-pointer shaadow-lg group overflow-hidden md:pl-0">
+                <div class="relative cursor-pointer shaadow-lg group overflow-hidden pl-6 md:pl-0 mb-6 md:mb-0 ">
                     <img src="https://cdn.shopify.com/s/files/1/0066/2326/4828/products/PufferJacket2_360x.png?v=1663679518"
                         alt="" class="h-96 duration-500 group-hover:opacity-0">
                     <img src="https://cdn.shopify.com/s/files/1/0066/2326/4828/products/image_67c584d0-d8d4-4452-8bd5-a360370b647d_360x.jpg?v=1662634229"
                         alt="" class="h-96 absolute top-0 z-[-1]">
-                    <!-- <p class="absolute px-3 group-hover:bottom-28 text-center bg-black text-white duration-700">
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    </p> -->
                     <a href="{{route('product.view')}}">
                         <button class="absolute px-5 py-2 bg-black text-white font-semibold rounded
                     left-1/4 bottom-[-65px] group-hover:bottom-14 duration-700">VIEW
                             PRODUCT</button>
                     </a>
-
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor"
+                        class="w-8 h-8 absolute bg-black text-white  rounded-full p-2 duration-500 right-[-60px] top-5 hover:bg-slate-800 group-hover:right-2 delay-100">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+                    </svg>
+                </div>
+                <div class="relative cursor-pointer shaadow-lg group overflow-hidden pl-6 md:pl-0 mb-6 md:mb-0">
+                    <img src="https://cdn.shopify.com/s/files/1/0066/2326/4828/products/PufferJacket2_360x.png?v=1663679518"
+                        alt="" class="h-96 duration-500 group-hover:opacity-0">
+                    <img src="https://cdn.shopify.com/s/files/1/0066/2326/4828/products/image_67c584d0-d8d4-4452-8bd5-a360370b647d_360x.jpg?v=1662634229"
+                        alt="" class="h-96 absolute top-0 z-[-1]">
+                    <a href="{{route('product.view')}}">
+                        <button class="absolute px-5 py-2 bg-black text-white font-semibold rounded
+                    left-1/4 bottom-[-65px] group-hover:bottom-14 duration-700">VIEW
+                            PRODUCT</button>
+                    </a>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="currentColor"
                         class="w-8 h-8 absolute bg-black text-white  rounded-full p-2 duration-500 right-[-60px] top-5 hover:bg-slate-800 group-hover:right-2 delay-100">
@@ -153,14 +168,16 @@
                 </div>
 
 
-                <div class="relative cursor-pointer shaadow-lg group overflow-hidden pl-6 md:pl-0">
+                <div class="relative cursor-pointer shaadow-lg group overflow-hidden pl-6 md:pl-0 mb-6 md:mb-0">
                     <img src="https://cdn.shopify.com/s/files/1/0066/2326/4828/products/PufferJacket2_360x.png?v=1663679518"
                         alt="" class="h-96 duration-500 group-hover:opacity-0">
                     <img src="https://cdn.shopify.com/s/files/1/0066/2326/4828/products/image_67c584d0-d8d4-4452-8bd5-a360370b647d_360x.jpg?v=1662634229"
                         alt="" class="h-96 absolute top-0 z-[-1]">
-                    <button class="absolute px-5 py-2 bg-black text-white font-semibold rounded
-                left-1/4 bottom-[-65px] group-hover:bottom-14 duration-700">
-                        VIEW PRODUCT</button>
+                    <a href="{{route('product.view')}}">
+                        <button class="absolute px-5 py-2 bg-black text-white font-semibold rounded
+                    left-1/4 bottom-[-65px] group-hover:bottom-14 duration-700">VIEW
+                            PRODUCT</button>
+                    </a>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="currentColor"
                         class="w-8 h-8 absolute bg-black text-white  rounded-full p-2 duration-500 right-[-60px] top-5 hover:bg-slate-800 group-hover:right-2 delay-100">
@@ -170,29 +187,16 @@
                 </div>
 
 
-                <div class="relative cursor-pointer shaadow-lg group overflow-hidden pl-6 md:pl-0">
+                <div class="relative cursor-pointer shaadow-lg group overflow-hidden pl-6 md:pl-0 mb-6 md:mb-0">
                     <img src="https://cdn.shopify.com/s/files/1/0066/2326/4828/products/PufferJacket2_360x.png?v=1663679518"
                         alt="" class="h-96 duration-500 group-hover:opacity-0">
                     <img src="https://cdn.shopify.com/s/files/1/0066/2326/4828/products/image_67c584d0-d8d4-4452-8bd5-a360370b647d_360x.jpg?v=1662634229"
                         alt="" class="h-96 absolute top-0 z-[-1]">
-                    <button class="absolute px-5 py-2 bg-black text-white font-semibold rounded
-                left-1/4 bottom-[-65px] group-hover:bottom-14 duration-700">VIEW PRODUCT</button>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor"
-                        class="w-8 h-8 absolute bg-black text-white  rounded-full p-2 duration-500 right-[-60px] top-5 hover:bg-slate-800 group-hover:right-2 delay-100">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
-                    </svg>
-                </div>
-
-
-                <div class="relative cursor-pointer shaadow-lg group overflow-hidden pl-6 md:pl-0">
-                    <img src="https://cdn.shopify.com/s/files/1/0066/2326/4828/products/PufferJacket2_360x.png?v=1663679518"
-                        alt="" class="h-96 duration-500 group-hover:opacity-0">
-                    <img src="https://cdn.shopify.com/s/files/1/0066/2326/4828/products/image_67c584d0-d8d4-4452-8bd5-a360370b647d_360x.jpg?v=1662634229"
-                        alt="" class="h-96 absolute top-0 z-[-1]">
-                    <button class="absolute px-5 py-2 bg-black text-white font-semibold rounded
-                left-1/4 bottom-[-65px] group-hover:bottom-14 duration-700">VIEW PRODUCT</button>
+                    <a href="{{route('product.view')}}">
+                        <button class="absolute px-5 py-2 bg-black text-white font-semibold rounded
+                    left-1/4 bottom-[-65px] group-hover:bottom-14 duration-700">VIEW
+                            PRODUCT</button>
+                    </a>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="currentColor"
                         class="w-8 h-8 absolute bg-black text-white  rounded-full p-2 duration-500 right-[-60px] top-5 hover:bg-slate-800 group-hover:right-2 delay-100">
@@ -203,4 +207,28 @@
             </div>
         </div>
     </section>
+
+
 </x-app-layout>
+
+
+<!-- <div class="relative cursor-pointer shaadow-lg group overflow-hidden md:pl-0">
+    <img src="https://cdn.shopify.com/s/files/1/0066/2326/4828/products/PufferJacket2_360x.png?v=1663679518" alt=""
+        class="h-96 duration-500 group-hover:opacity-0">
+    <img src="https://cdn.shopify.com/s/files/1/0066/2326/4828/products/image_67c584d0-d8d4-4452-8bd5-a360370b647d_360x.jpg?v=1662634229"
+        alt="" class="h-96 absolute top-0 z-[-1]"> -->
+<!-- <p class="absolute px-3 group-hover:bottom-28 text-center bg-black text-white duration-700">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    </p> -->
+<!-- <a href="{{route('product.view')}}">
+    <button class="absolute px-5 py-2 bg-black text-white font-semibold rounded
+                    left-1/4 bottom-[-65px] group-hover:bottom-14 duration-700">VIEW
+        PRODUCT</button>
+</a>
+
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+    class="w-8 h-8 absolute bg-black text-white  rounded-full p-2 duration-500 right-[-60px] top-5 hover:bg-slate-800 group-hover:right-2 delay-100">
+    <path stroke-linecap="round" stroke-linejoin="round"
+        d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+</svg>
+</div> -->
