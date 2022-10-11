6e7932298f89c62459b6989dbef6c35dfe14310b
Favour <toluwaniokunowo@gmail.com>
2022-10-11 10:37:18 +0100

    sticky header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,13 +13,20 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@200;300;400;500&display=swap" rel="stylesheet">
+
+    <x-livewire-alert::scripts />
+
+    @livewireStyles
+
+    @livewireScripts
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body>
     @include('layouts.header')
 
-    <main>
+    <main class="relative">
         {{ $slot }}
     </main>
 
