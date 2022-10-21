364402f3bac87084f6dc218fbbd2be69ef1b119a
Favour <toluwaniokunowo@gmail.com>
2022-10-21 17:58:57 +0100

    fix wrong colour showing on product edit form

--- resources/views/components/color.blade.php
+++ resources/views/components/color.blade.php
@@ -1,14 +1,14 @@
-<div x-data="{ color: '#000000' }" x-init="
+<div x-data="{ colour: @entangle($attributes->wire('model')) }" x-init="
         picker = new Picker($refs.button);
         picker.onDone = rawColor => {
-            color = rawColor.hex;
-            $dispatch('input', color)
+            colour = rawColor.hex;
+            $dispatch('input', colour)
         }
     " wire:ignore {{ $attributes }}>
 
-    <button x-ref="button" class="w-full">
+    <button x-ref="button" class="w-full h-full">
         <span class="block mt-1 text-left w-full rounded-md form-input focus:border-indigo-600"
-            x-text="color" :style="`background: ${color}`">
+            x-text="colour" :style="`background: ${colour}`">
         </span>
     </button>
 </div>
