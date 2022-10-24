e684e2bfff84234aad95c4b173e3e7ce18214d63
Favour <toluwaniokunowo@gmail.com>
2022-10-24 21:02:11 +0100

    add social icon links

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -4,19 +4,8 @@
         <div class="py-4 mt-8 border-t border-gray-700 md:flex md:items-center md:justify-between">
             <x-application-logo class="w-10 h-10 fill-current text-gray-500" />
 
-            <div class="flex space-x-6 md:order-2">
-                <a href="#">
-                    <span class="sr-only">Facebook</span>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24"
-                        height="24">
-                        <path fill="none" d="M0 0h24v24H0z" />
-                        <path
-                            d="M15.402 21v-6.966h2.333l.349-2.708h-2.682V9.598c0-.784.218-1.319 1.342-1.319h1.434V5.857a19.19 19.19 0 0 0-2.09-.107c-2.067 0-3.482 1.262-3.482 3.58v1.996h-2.338v2.708h2.338V21H4a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h16a1 1 0 0 1 1 1v16a1 1 0 0 1-1 1h-4.598z"
-                            fill="rgba(51,78,224,1)" />
-                    </svg>
-                </a>
-
-                <a href="#">
+            <div class="flex space-x-6 md:order-2 items-center">
+                <a href="https://instagram.com/Drip__with__piper" target="_blank">
                     <span class="sr-only">Instagram</span>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24"
                         height="24">
@@ -27,7 +16,7 @@
                     </svg>
                 </a>
 
-                <a href="#">
+                <a href="https://twitter.com/Drip_Piper" target="_blank">
                     <span class="sr-only">Twitter</span>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24"
                         height="24">
@@ -38,18 +27,13 @@
                     </svg>
                 </a>
 
-                <a href="#">
+                <a href="https://wa.me/08101434302" target="_blank">
                     <span class="sr-only">WhatsApp</span>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24"
-                        height="24">
-                        <path fill="none" d="M0 0h24v24H0z" />
-                        <path
-                            d="M7.253 18.494l.724.423A7.953 7.953 0 0 0 12 20a8 8 0 1 0-8-8c0 1.436.377 2.813 1.084 4.024l.422.724-.653 2.401 2.4-.655zM2.004 22l1.352-4.968A9.954 9.954 0 0 1 2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10a9.954 9.954 0 0 1-5.03-1.355L2.004 22zM8.391 7.308c.134-.01.269-.01.403-.004.054.004.108.01.162.016.159.018.334.115.393.249.298.676.588 1.357.868 2.04.062.152.025.347-.093.537a4.38 4.38 0 0 1-.263.372c-.113.145-.356.411-.356.411s-.099.118-.061.265c.014.056.06.137.102.205l.059.095c.256.427.6.86 1.02 1.268.12.116.237.235.363.346.468.413.998.75 1.57 1l.005.002c.085.037.128.057.252.11.062.026.126.049.191.066a.35.35 0 0 0 .367-.13c.724-.877.79-.934.796-.934v.002a.482.482 0 0 1 .378-.127c.06.004.121.015.177.04.531.243 1.4.622 1.4.622l.582.261c.098.047.187.158.19.265.004.067.01.175-.013.373-.032.259-.11.57-.188.733a1.155 1.155 0 0 1-.21.302 2.378 2.378 0 0 1-.33.288 3.71 3.71 0 0 1-.125.09 5.024 5.024 0 0 1-.383.22 1.99 1.99 0 0 1-.833.23c-.185.01-.37.024-.556.014-.008 0-.568-.087-.568-.087a9.448 9.448 0 0 1-3.84-2.046c-.226-.199-.435-.413-.649-.626-.89-.885-1.562-1.84-1.97-2.742A3.47 3.47 0 0 1 6.9 9.62a2.729 2.729 0 0 1 .564-1.68c.073-.094.142-.192.261-.305.127-.12.207-.184.294-.228a.961.961 0 0 1 .371-.1z"
-                            fill="rgba(53,195,21,1)" />
-                    </svg>
+                    <img alt="Chat on WhatsApp"
+                        src="{{ asset('images/WhatsAppButtonGreenSmall.png') }}" />
                 </a>
             </div>
-            <p class="mt-8 text-base text-white md:mt-0 md:order-1">&copy; 2022 PiperDreams.
+            <p class="mt-8 text-base text-white md:mt-0 md:order-1">&copy; 2022 DripPiper.
                 All rights reserved.</p>
         </div>
     </div>
